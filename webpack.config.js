1c79ae8c1aa5ffee4f8fa318f5f9d53b00269097
Viorel Strogoteanu <viorelss@icloud.com>
2024-10-02 16:11:42 +0200

    Configured webpack mode for production

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         bundle: path.resolve(__dirname, 'src/index.js'),
     },
