a0d1a360b5983e5f39ff4c089ac3de5c8b4186a7
Viorel Strogoteanu <viorelss@icloud.com>
2024-10-02 16:05:54 +0200

    Updated css load

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         rules: [
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.js$/,
